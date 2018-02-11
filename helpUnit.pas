04c2a314c788b26128edf15d56d7638ca87714e7
Valentin <prelovskyvalentine@gmail.com>
2018-02-11 12:26:41 +0800

    Добавлена функция scale в процентах
    Источник - drkb

--- helpUnit.pas
+++ helpUnit.pas
@@ -8,9 +8,37 @@ interface
 function appPath(): string; 
 function getFunction(item: string): string;
 function getInfo(item: string):string;
+function ScalePercentBmp(bitmp: TBitmap; iPercent: Integer): Boolean;
 //----------------------------------------------------------------------------
 implementation
-
+function ScalePercentBmp(bitmp: TBitmap; iPercent: Integer): Boolean;
+var
+TmpBmp: TBitmap;
+ARect: TRect;
+h, w: Real;
+hi, wi: Integer;
+begin
+Result := False;
+try
+   TmpBmp := TBitmap.Create;
+   try
+     h := bitmp.Height * (iPercent / 100);
+     w := bitmp.Width * (iPercent / 100);
+     hi := StrToInt(FormatFloat('#', h)) + bitmp.Height;
+     wi := StrToInt(FormatFloat('#', w)) + bitmp.Width;
+     TmpBmp.Width := wi;
+     TmpBmp.Height := hi;
+     ARect := Rect(0, 0, wi, hi);
+     TmpBmp.Canvas.StretchDraw(ARect, Bitmp);
+     bitmp.Assign(TmpBmp);
+   finally
+    TmpBmp.Free;
+   end;
+   Result := True;
+except
+   Result := False;
+end;
+end;
 function getInfo(item: string):string;
 var pos: integer;
 begin
