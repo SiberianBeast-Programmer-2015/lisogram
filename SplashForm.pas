b4fe6d7775df5df1222fa674734c41ab3cdb92be
Valentin <prelovskyvalentine@gmail.com>
2018-02-17 13:01:30 +0800

    Растянул на весь экран

--- SplashForm.pas
+++ SplashForm.pas
@@ -4,12 +4,14 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, OleCtrls, WMPLib_TLB;
+  Dialogs, OleCtrls, WMPLib_TLB,helpUnit;
 
 type
   TForm3 = class(TForm)
     WindowsMediaPlayer1: TWindowsMediaPlayer;
     procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FormResize(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,7 +27,25 @@ implementation
 
 procedure TForm3.FormCreate(Sender: TObject);
 begin
+  WindowsMediaPlayer1.Width  := Form3.Width;
+  WindowsMediaPlayer1.Height := Form3.Height;
+  WindowsMediaPlayer1.Left := 0;
+  WindowsMediaPlayer1.top := 0;
   WindowsMediaPlayer1.uiMode := 'none';
+  
+  WindowsMediaPlayer1.URL := appData + 'splash.avi';  
+end;
+
+procedure TForm3.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  WindowsMediaPlayer1.controls.stop;
+  WindowsMediaPlayer1.Close;
+end;
+
+procedure TForm3.FormResize(Sender: TObject);
+begin
+  WindowsMediaPlayer1.Width := Form3.Width - 10;
+  WindowsMediaPlayer1.Height := Form3.Height - 10;
 end;
 
 end.
